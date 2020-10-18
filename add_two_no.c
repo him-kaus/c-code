a6bbced8052a3f44a31149cba72a549f275b3063
him-kaus <72623413+him-kaus@users.noreply.github.com>
2020-10-18 12:34:23 +0530

    Update add_two_no.c

--- add_two_no.c
+++ add_two_no.c
@@ -1,23 +1,9 @@
 #include<stdio.h>
 int main()
 {
-	int n,i,fact;
-	printf("Enter a no:");
-	scanf("%d",&n);
-	/*for(i=1;i<=10;i++)
-	{
-	fact =n%i;
-	pritf("fact:%d",fact);	
-	}
-	*/
-	if(n%i==0)
-	{
-	printf("No. is not prime.. ");
-	}
-	else
-	{
-		printf("No is prime..");
-	}
-	
-	 return 0;
+	int a,b;
+	printf("enter a two number");
+	scanf("%d%d",&a,&b);
+	printf("sum of %d and %d is %d",a,b,a+b);
+	return 0;
 }
