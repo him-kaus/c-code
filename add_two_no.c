c5a8cc715a78701057670db21bb513cd697fdf33
him-kaus <72623413+him-kaus@users.noreply.github.com>
2020-10-18 12:24:10 +0530

    Add files via upload
    addition of two no.

---
+++ add_two_no.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,fact;
+	printf("Enter a no:");
+	scanf("%d",&n);
+	/*for(i=1;i<=10;i++)
+	{
+	fact =n%i;
+	pritf("fact:%d",fact);	
+	}
+	*/
+	if(n%i==0)
+	{
+	printf("No. is not prime.. ");
+	}
+	else
+	{
+		printf("No is prime..");
+	}
+	
+	 return 0;
+}
