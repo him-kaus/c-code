2b2ce5153b636d3949dd1194ba81484db92b12c4
notaweed <91742459+notaweed@users.noreply.github.com>
2021-10-01 20:01:26 +0530

    Update and rename area find in c. to calculating_area.c

--- calculating_area.c
+++ calculating_area.c
@@ -7,17 +7,17 @@ int main()
 	printf("\n enter a value");
 	scanf("%d",&r);
 	circle=3.14*r*r;
-	printf("area of circle=%f\n",circle);
+	printf("area of circle=%.2f\n",circle);
 	
 	printf("\n enter a value");
 	scanf("%d",&a);
 	squar=a*a;
-	printf("area of squar=%f\n",squar);
+	printf("area of squar=%.2f\n",squar);
 	
 	printf("\n enter two values");
 	scanf("%d %d",&bs,&ht);
 	triangle=0.5*bs*ht;
-	printf("area of triangle=%f",triangle);
+	printf("area of triangle=%.2f",triangle);
 	
 	return 0;
 }
