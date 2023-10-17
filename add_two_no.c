e7a76cd4a4b2400cb38d18db699b54f5ed542ce2
Gunjan-Rajput <116620443+Gunjan-Rajput@users.noreply.github.com>
2023-10-17 07:09:38 +0530

    Update add two integers .c

--- add_two_no.c
+++ add_two_no.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 int main()
 {
-	int a,b;
+	int a,b,sum;
 	printf("enter a two number");
 	scanf("%d%d",&a,&b);
-	printf("sum of %d and %d is %d",a,b,a+b);
+	sum=a+b;
+	printf(" %d + %d= %d",a,b,sum);
 	return 0;
 }
